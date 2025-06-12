220e8ba9ccb46de358752c6ce52bf6dadf5f1221
陳曦 <eriksonchen@gmail.com>
2025-06-12 15:46:17 +0200

    Merge Kana and Furigana

--- src/components/Kana.jsx
+++ src/components/Kana.jsx
@@ -1,18 +1,51 @@
 import React, { useState } from 'react';
 
 // an inline element marked w/ accent, changes type on click 
-export default function Kana({text = ''}) {
+export default function Kana({ text, onUpdate, editable = false}) {
     const [type, setType] = useState(0);
+    // if editable, set firstClick to true
+    // this is to block the first click from changing the type
+    const [firstClick, setFirstClick] = useState(editable);
     const typeName = ['none', 'flat', 'drop'];
-    const changeType = () => {
-        setType(prev => (prev + 1) % 3);
-    };
     
+    const changeType = () => {
+        // if it's the first click that will start an edit, don't change type
+        setType(prev => (prev + 1 - firstClick) % 3);
+        // it isn't the first click anymore till finish editing
+        setFirstClick(false);
+    };
+
+    let finishEditing = e => {
+        if (!editable) return;
+        onUpdate?.(e.target.innerText)
+        // note that if the Kana isn't editable, firstClick will never be ture
+        setFirstClick(true);
+    };
+
+    let handleKeyDown = e => {
+        if (e.key === 'Backspace') {
+            if (e.target.innerText.length <= 1)
+                e.target.innerText = '\u00A0'; // leave a placeholder space
+        }
+        if (e.key === 'Enter' && !e.shiftKey) {
+            e.preventDefault();
+            if (e.target.innerText.length == 0)
+                e.target.innerText = '\u00A0'; 
+            if (e.target.innerText === '\u00A0') 
+                setType(0); // reset type if empty
+            e.target.blur(); // trigger onBlur and save
+        }
+    };
+
     return (
         // receive accent class only when type is non-zero
         <span 
-            className={type && `accent-${typeName[type]}`} 
-            onClick={changeType} 
+            className={`${type && `accent-${typeName[type]}`} ${editable && 'furigana'}`} 
+            onClick={changeType}
+            contentEditable={editable}
+            suppressContentEditableWarning
+            onBlur={finishEditing}
+            onKeyDown={handleKeyDown}
         >
             {text}
         </span>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 
 import Kana from 'components/Kana.jsx';
-import Furigana from 'components/Furigana.jsx';
 import 'components/Main.css';
 import 'utilities/accentMarker.css';
 
@@ -12,7 +11,9 @@ export default function MainPage(props) {
     // Initial paragraph and words state, the placeholder text is just for testing
     const [paragraph, setParagraph] = useState("今日は久しぶりに晴れて、とても気持ちのいい天気だった。");
     // The placeholder furigana is set to 'あ' for all words, this can be changed later
-    const [words, setWords] = useState([...segmenter.segment(paragraph)].map(s => {return{surface: s.segment, furigana: 'あ'}}));
+    const [words, setWords] = useState([...segmenter.segment(paragraph)].map(s => 
+        {return{surface: s.segment, furigana: 'あ'}}
+    ));
     
     // On input change, update the paragraph and segment it into words
     let updateResult = e => {
@@ -45,11 +46,7 @@ export default function MainPage(props) {
                 {/* Display words according to surface and furigana */}
                 {words.map((word, index) => 
                     <ruby key={`${index}-${word}`}>
-                        {/* If there is furigana, the word is a kanji, 
-                            i.e. it shouldn't be able to be marked w/ an accent directly, 
-                            so we just display the kanji in <span>.
-                            Otherwise the word is pure kana, 
-                            display it w/ <Kana> to enable accent marking */}
+                        {/* Kanji -> <span>, kana -> <Kana> (accent enabled) */}
                         {[...word.surface].map((text, textIndex) =>
                             word.furigana ? 
                                 <span key={`${index}-${textIndex}`}>{text}</span> :
@@ -58,7 +55,8 @@ export default function MainPage(props) {
                         {/* If there is furigana, display it in rt and make it editable */}
                         <rt>
                             {word.furigana && 
-                                <Furigana 
+                                <Kana
+                                    editable
                                     text={word.furigana} 
                                     onUpdate={
                                         newFurigana => {
