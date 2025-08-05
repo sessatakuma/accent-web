a57fd13759de0092fe94463df2ffa94197854f35
陳曦 <eriksonchen@gmail.com>
2025-08-05 22:50:00 +0200

    working on breaking long furigana

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -7,6 +7,7 @@ import isKana from 'utilities/isKana.jsx';
 import Kana from 'components/Kana.jsx';
 import 'components/Result.css';
 import { placeholder } from 'utilities/placeholder.jsx';
+import { splitKanaSyllables } from 'utilities/kanaUtils.jsx';
 
 const Result = forwardRef(({words, setWords}, ref) => {
     const [showCopyDescription, setShowCopyDescription] = useState(false); 
@@ -97,8 +98,7 @@ const Result = forwardRef(({words, setWords}, ref) => {
 
     const updateFurigana = (wordIndex, textIndex, newFurigana, newAccent) => {
         let newWords = [...words];
-        let deleting = newFurigana === placeholder;
-        if (deleting) {
+        if (newFurigana === placeholder) { // 被刪空
             if (newWords[wordIndex].furigana.length === 1) { //　要刪光了就放placeholder
                 newWords[wordIndex].furigana[textIndex].text = placeholder;
                 newWords[wordIndex].furigana[textIndex].accent = 0;
@@ -106,9 +106,16 @@ const Result = forwardRef(({words, setWords}, ref) => {
             else // 不然刪掉
                 newWords[wordIndex].furigana.splice(textIndex, 1);
         }
-        else {
+        else if (splitKanaSyllables(newFurigana).length === 1) { // 音節長度還是1就直接更新
             newWords[wordIndex].furigana[textIndex].text = newFurigana;
             newWords[wordIndex].furigana[textIndex].accent = newAccent;
+        }
+        else { // 變長就把原本的刪掉把新的拆開丟進furigana
+            // newWords[wordIndex].furigana.splice(textIndex, 0, 
+            //     ...splitKanaSyllables(newFurigana).map(char => ({
+            //         text: char,
+            //         accent: 0
+            //     })));
         }
         setWords(newWords);
     }
