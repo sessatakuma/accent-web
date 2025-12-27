648f8ce1726a7eaff7c6abe3eb09a94b55af690a
陳曦 <eriksonchen@gmail.com>
2025-12-27 14:02:39 +0800

    feat: update global layout and styles for new design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,19 @@
 import React from "react";
-
+import { Youtube } from 'lucide-react';
 import 'components/Footer.css';
 
 export default function Footer() {
     return (
         <footer className='footer'>
             <span className="title">せっさたくま</span> 
-            <a className='youtube' href="https://www.youtube.com/@%E3%81%9B%E3%81%A3%E3%81%95%E3%81%9F%E3%81%8F%E3%81%BE">
-                <i className="fa-brands fa-youtube" />
+            <a 
+                className='youtube' 
+                href="https://www.youtube.com/@%E3%81%9B%E3%81%A3%E3%81%95%E3%81%9F%E3%81%8F%E3%81%BE" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                title="YouTubeチャンネル"
+            >
+                <Youtube size={24} />
             </a>
         </footer>
     );
