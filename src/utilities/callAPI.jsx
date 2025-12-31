08365ff1200d7227b1a3b936bf462a7b8e831aeb
陳曦 <eriksonchen@gmail.com>
2026-01-01 02:07:07 +0800

    fix: resolve eslint errors

--- src/utilities/callAPI.jsx
+++ src/utilities/callAPI.jsx
@@ -1,13 +1,12 @@
 import isKana from 'utilities/isKana.jsx';
 import { splitKanaSyllables } from 'utilities/kanaUtils.jsx';
-import { placeholder } from 'utilities/placeholder.jsx';
 
 export async function fetchFuriganaFromAPI(text) {
     try {
         const response = await fetch('https://api.mygo.page/api/MarkAccent/', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ text })
+            body: JSON.stringify({ text }),
         });
 
         const data = await response.json();
@@ -16,10 +15,8 @@ export async function fetchFuriganaFromAPI(text) {
             // TODO: add placeholder for foreign words
             return data.result.map(entry => {
                 let furigana = entry.furigana;
-                if (isKana(entry.surface)) 
-                    furigana = '';
-                else 
-                    furigana = splitKanaSyllables(furigana);
+                if (isKana(entry.surface)) furigana = '';
+                else furigana = splitKanaSyllables(furigana);
                 return {
                     surface: entry.surface,
                     furigana,
