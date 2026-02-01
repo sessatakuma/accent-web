bf242673be8690b9075782aa515fe14eadb0af9d
陳曦 <eriksonchen@gmail.com>
2026-02-01 18:53:19 +0800

    chore: format files

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
 
 import Main from 'components/Main.jsx';
+import { createRoot } from 'react-dom/client';
 
 import './index.css';
 
-window.onload = function() {
+window.onload = function () {
     const container = document.getElementById('root');
     const root = createRoot(container);
-    root.render(<Main/>);
+    root.render(<Main />);
 };
